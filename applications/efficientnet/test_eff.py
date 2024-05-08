448b18e376380dd0263688d16286e6f3c424bfcf
jacky <jacky@xx.com>
2024-05-08 17:31:57 +0800

    update

--- applications/efficientnet/test_eff.py
+++ applications/efficientnet/test_eff.py
@@ -12,7 +12,7 @@ class TestEffDms(unittest.TestCase):
                 dtp_mutator_cfg=dict(
                     parse_cfg=dict(
                         demo_input=dict(
-                            input_shape=(1, 3, 240, 240),
+                            input_shape=(1, 3, 224, 224),
                         ),
                     )
                 ),
