6ba9f636bf3b548a580a90f4730b7f86f31b9159
jacky <jacky@xx.com>
2024-05-16 11:06:56 +0800

    update

--- applications/efficientnet/scripts/DMS-450/prune.sh
+++ applications/efficientnet/scripts/DMS-450/prune.sh
@@ -1,6 +1,6 @@
 torchrun --nproc_per_node=8 --master_port 29112  timm_pruning.py ./data/imagenet_torch --model efficientnet_b4 -b 96 --sched step --epochs 4 \
 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 \
 --drop 0.5 --drop-path 0.2 --model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5 --remode pixel --reprob 0.2 --amp --lr .048 \
---experiment 7b_to_45_low --pin-mem   --input-size 3 224 224 \
+--experiment dms_450_pruned --pin-mem   --input-size 3 224 224 \
 --target 0.296 --mutator_lr  2e-5 --loss_weight 1 --skip_full_target 
 
--- applications/efficientnet/scripts/DMS-450/retrain.sh
+++ applications/efficientnet/scripts/DMS-450/retrain.sh
@@ -1,5 +1,5 @@
 torchrun --nproc_per_node=8 --master_port 29112 \
 timm_retrain.py ./data/imagenet_torch --model efficientnet_b4 -b 96 --sched step --epochs 450 --decay-epochs 2.4 --decay-rate .97 --opt rmsproptf --opt-eps .001 -j 8 --warmup-lr 1e-6 --weight-decay 1e-5 --drop 0.2 --drop-path 0.2 --model-ema --model-ema-decay 0.9999 --aa rand-m9-mstd0.5 --remode pixel --reprob 0.2 --amp --lr .048 \
---experiment retain_45 --pin-mem --resume output/train/retain_45/last.pth.tar  \
---pruned checkpoints/450/pruned.pth --input-size 3 224 224 --target 0.2924 --teacher timm/efficientnet_b4 --teacher_input_image_size 320
+--experiment dms_450_retrain_distil --pin-mem --resume output/train/dms_450_retrain_distil/last.pth.tar  \
+--pruned output/train/dms_450_prune/last.pth.tar --input-size 3 224 224 --target 0.2924 --teacher timm/efficientnet_b4 --teacher_input_image_size 320
 
